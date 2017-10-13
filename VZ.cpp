cb5c6694e86d4a1721d45a217b3c8e5dc8df3a40
ArtemTszyu <32194510+ArtemTszyu@users.noreply.github.com>
2017-10-13 12:23:54 +0300

    Update VZ.cpp

--- VZ.cpp
+++ VZ.cpp
@@ -3,28 +3,28 @@
 using namespace std;
 
 int main (){
-	int a[10]; int i, j; int p=0;
+	int a[10]; int i, j, r; 
+	int p=0;
   string stroka;
 	getline(cin, stroka);
 	istringstream stream(stroka);
 	for(i=0; i<10; ++i){
 	    if(!(stream>>a[i])){
 	        cout<<"An error has occurred while reading the input sequence"<<endl;
-	        return -1;
+	        return 0;
 	    }
 	    if(p>a[i]){
 	        cout<<"The input sequence must be non-decreasing"<<endl;
-	        return -1;
+	        return 0;
 	    }
 	    p=a[i];
-	}
-	int r;
+	}	
 	cin>>r;
 	for(i=0; i<10; ++i){
 	    for(j=0; j<10; ++j){
 	        if((a[i]+a[j])==r){
 	            cout<<a[i]<<" "<<a[j];
-	            return -1;
+	            return 0;
 	        }
 	    }
 	}
